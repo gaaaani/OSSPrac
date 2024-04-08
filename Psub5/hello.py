810443a61fe1d80f4dc9b93af8c4403fe2caeb03
gaaaani <geseo0523@gmail.com>
2024-04-08 18:57:19 +0900

    개인과제5 re ossp1 branch

--- Psub5/hello.py
+++ Psub5/hello.py
@@ -1,6 +1,6 @@
 Name = input("이름을 입력하세요 : ")
 Id = input("학번을 입력하세요 : ")
-
+Dept = input("학과를 입력하세요 : ")
 
 print("<출력>")
-print("이름 : ", Name , "\n학번 : ", Id)
+print("이름 : ", Name , "\n학번 : ", Id, "\n학과 : ", Dept)
